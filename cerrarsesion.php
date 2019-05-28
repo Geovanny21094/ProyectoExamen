f8df4f970e5ede7e2ed7d4ccc7d974f23b3c8c06
Geovanny Duchitanga Abad <gioabad210@hotmail.com>
2019-05-28 11:35:15 -0500

    actualizacion

--- cerrarsesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	session_destroy();	// <-- DESTRUYE LA SESION EN TODAS LAS PAGINAS DONDE SE HAYA INICIADO LA SESION
-	header("location: index.php"); //<-- Redirecciona a la pagina inicial de LOGIN
- ?>
